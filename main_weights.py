471da40eb8c2dfd91ae676cfda565256e29e7e65
Christopher Morris <christopher.morris@udo.edu>
2022-09-10 21:09:00 +0200

    Simpel GNN implementation for playing around.

--- main_weights.py
+++ main_weights.py
@@ -7,8 +7,65 @@ import seaborn as sns
 
 from torch_geometric.datasets import TUDataset
 from torch_geometric.loader import DataLoader
-from torch_geometric.nn import MLP, global_add_pool, GraphConv
+from torch_geometric.nn import MLP, global_add_pool
 import matplotlib.pyplot as plt
+
+from typing import Tuple, Union
+
+from torch import Tensor
+from torch_sparse import SparseTensor, matmul
+
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.typing import Adj, OptPairTensor, OptTensor, Size
+
+
+class GraphConv(MessagePassing):
+    def __init__(
+        self,
+        in_channels,
+        out_channels):
+        super().__init__(aggr='add')
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        if isinstance(in_channels, int):
+            in_channels = (in_channels, in_channels)
+
+        self.lin_rel = Linear(in_channels[0], out_channels, bias=True)
+        self.lin_root = Linear(in_channels[1], out_channels, bias=False)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lin_rel.reset_parameters()
+        self.lin_root.reset_parameters()
+
+    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
+                edge_weight: OptTensor = None, size: Size = None) -> Tensor:
+        """"""
+        if isinstance(x, Tensor):
+            x: OptPairTensor = (x, x)
+
+        # propagate_type: (x: OptPairTensor, edge_weight: OptTensor)
+        out = self.propagate(edge_index, x=x, edge_weight=edge_weight,
+                             size=size)
+        out = self.lin_rel(out)
+
+        x_r = x[1]
+        if x_r is not None:
+            out += self.lin_root(x_r)
+
+        return out
+
+    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
+        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor,
+                              x: OptPairTensor) -> Tensor:
+        return matmul(adj_t, x[0], reduce=self.aggr)
+
 
 batch_size = 128
 num_layers = 5
@@ -26,7 +83,7 @@ class Net(torch.nn.Module):
 
         self.convs = torch.nn.ModuleList()
         for _ in range(nc):
-            self.convs.append(GraphConv(in_channels, hidden_channels, aggr = 'add', bias = True))
+            self.convs.append(GraphConv(in_channels, hidden_channels))
             in_channels = hidden_channels
 
         # TODO: No dropout.
