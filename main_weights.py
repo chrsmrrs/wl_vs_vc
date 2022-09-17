e96c9a09e6743abee79e17373ab166933b5d600b
Christopher Morris <christopher.morris@udo.edu>
2022-09-17 19:40:55 +0200

    Code for num. parameter experiments.

--- main_weights.py
+++ main_weights.py
@@ -157,7 +157,7 @@ for dataset_name in dataset_name_list:
         #                   y='diff',
         #                   data=data, color=colors[i], linestyle='--')
 
-        ax.set(xlabel='Epoch', ylabel='Accuracy [%]')
+        ax.set(xlabel='Epoch', ylabel= 'train - test accuracy [%]')
 
     table_data = np.array(table_data)
 
