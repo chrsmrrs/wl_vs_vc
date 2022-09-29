84603fe8b3ef26bfb6c502487776cac393d5e32d
Christopher Morris <christopher.morris@udo.edu>
2022-09-29 23:00:52 +0200

    Started working on size generaliztion.

--- colors_GNN/main_colors.py
+++ colors_GNN/main_colors.py
@@ -16,7 +16,7 @@ batch_size = 128
 num_layers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 lr = 0.001
 epochs = 5
-dataset_name_list = ["ENZYMES", "Mutagenicity", "NCI1", "NCI109", "MCF-7", "MCF-7H"]
+dataset_name_list = ["MCF-7", "MCF-7H"]
 num_reps = 5
 
 color_counts = [
