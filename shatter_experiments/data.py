66e900b221b3e28fe5ac6740d7744e5fcf42f5fc
toensoff <toenshoff@informatik.rwth-aachen.de>
2023-01-24 19:17:36 +0100

    cleaned shatter exp

--- shatter_experiments/data.py
+++ shatter_experiments/data.py
@@ -1,54 +1,9 @@
 import numpy as np
 import torch
 from torch_geometric.data import Data
-from torch_geometric.utils import to_networkx
-import networkx as nx
 
 
-def random_tree(order):
-    # build edges from depth 1 to root
-    num_depth_1 = int(np.sqrt(order))
-    src_depth_1 = torch.arange(1, num_depth_1 + 1)
-    tgt_depth_1 = torch.zeros((num_depth_1,), dtype=torch.long)
-    edges_depth_1 = torch.stack([src_depth_1, tgt_depth_1], dim=0)
-
-    # sample edges from depth 2 to depth 1
-    num_depth_2 = order - num_depth_1 - 1
-    src_depth_2 = torch.arange(num_depth_1 + 1, order)
-    tgt_depth_2 = torch.randint(1, num_depth_1 + 1, (num_depth_2,))
-    edges_depth_2 = torch.stack([src_depth_2, tgt_depth_2], dim=0)
-
-    edge_index = torch.cat([edges_depth_1, edges_depth_2], dim=1)
-
-    x = torch.ones((order,), dtype=torch.float32)
-    data = Data(x=x, edge_index=edge_index)
-    return data
-
-
-def get_wl_hash(data):
-    G = to_networkx(data)
-    wl_hash = nx.algorithms.weisfeiler_lehman_graph_hash(G)
-    return wl_hash
-
-
-def get_unique_trees(order, num_trees):
-    data_list = []
-    wl_hashes = set()
-
-    tries = 0
-    while len(data_list) < num_trees:
-        data = random_tree(order)
-        wl_hash = get_wl_hash(data)
-        if wl_hash not in wl_hashes:
-            data_list.append(data)
-            wl_hashes |= {wl_hash}
-        tries += 1
-        if tries > 10 * num_trees:
-            raise RuntimeError(f'Could not find {num_trees} trees of order {order}')
-    return data_list
-
-
-def simple_tree(deg_1, deg_2):
+def tnm_tree(deg_1, deg_2):
     # build edges from depth 1 to root
     src_depth_1 = torch.arange(1, 3)
     tgt_depth_1 = torch.zeros((2,), dtype=torch.long)
@@ -70,12 +25,12 @@ def simple_tree(deg_1, deg_2):
     return data
 
 
-def get_simple_trees(order):
+def get_tnm_trees(order):
     data_list = []
     max_idx = int(np.floor(order / 2))
     for i in range(max_idx+1):
         deg_1 = i
         deg_2 = order - i
-        data = simple_tree(deg_1, deg_2)
+        data = tnm_tree(deg_1, deg_2)
         data_list.append(data)
     return data_list
--- shatter_experiments/run_shatter_experiment.py
+++ shatter_experiments/run_shatter_experiment.py
@@ -7,23 +7,21 @@ import torch
 from torch_geometric.data import Batch
 
 from model import GNN
-from data import get_unique_trees
+from data import get_tnm_trees
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--num_layers", type=int, default=[3], nargs='+', help="Number of layers")
-    parser.add_argument("--hidden_dim", type=int, default=[64, 128, 256, 512], nargs='+', help="Hidden Dimension")
+    parser.add_argument("--hidden_dim", type=int, default=[4, 16, 64, 256], nargs='+', help="Hidden Dimension")
     parser.add_argument("--lr", type=float, default=1.e-4, help="Learning Rate")
-    parser.add_argument("--weight_decay", type=float, default=1.e-5, help="Weight Decay")
+    parser.add_argument("--weight_decay", type=float, default=0, help="Weight Decay")
     parser.add_argument("--pool_fn", type=str, default='sum', help="Pool")
-    parser.add_argument("--order", type=int, default=[15, 20, 25, 30, 35, 40], nargs='+', help="Order of trees")
-    parser.add_argument("--num_trees", type=int, default=[5, 10, 20, 30, 40, 50, 60, 70], nargs='+', help="Number of trees")
-    parser.add_argument("--repeat", type=int, default=4, help="Repetitions")
-    parser.add_argument("--num_assign", type=int, default=4, help="Number of assignments to try per set of trees")
-    parser.add_argument("--train_steps", type=int, default=10000, help="Number of steps per epoch")
-    parser.add_argument("--file_name", type=str, default='results.csv', help="CSV file to store results")
-    parser.add_argument("--seed", type=int, default=0, help="the random seed for torch and numpy")
+    parser.add_argument("--order", type=int, default=[10, 20, 30, 40, 50, 60, 70, 80, 90], nargs='+', help="Order of trees")
+    parser.add_argument("--num_assign", type=int, default=50, help="Number of assignments to try per set of trees")
+    parser.add_argument("--train_steps", type=int, default=100000, help="Number of steps per epoch")
+    parser.add_argument("--file_name", type=str, default='simple_tree_results.csv', help="CSV file to store results")
+    parser.add_argument("--seed", type=int, default=6, help="the random seed for torch and numpy")
     args = parser.parse_args()
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
@@ -36,38 +34,29 @@ if __name__ == '__main__':
     results = []
 
     for o in args.order:
-        for t in args.num_trees:
-            for r in range(args.repeat):
-                try:
-                    data_list = get_unique_trees(o, t)
-                    data = Batch.from_data_list(data_list)
-                except RuntimeError:
-                    print(f'Could not find {t} trees with order {o}')
-                    break
+        data_list = get_tnm_trees(o - 3)
+        data = Batch.from_data_list(data_list)
+        for a in range(args.num_assign):
+            data.y = torch.randint(0, 2, (len(data_list),), device=data.x.device)
 
-                for a in range(args.num_assign):
-                    data.y = torch.randint(0, 2, (t,), device=data.x.device).float()
+            for h in args.hidden_dim:
+                for l in args.num_layers:
+                    model = GNN(in_dim=1, out_dim=1, hidden_dim=h, num_layers=l, pool_fn=args.pool_fn)
 
-                    for h in args.hidden_dim:
-                        for l in args.num_layers:
-                            model = GNN(in_dim=1, out_dim=1, hidden_dim=h, num_layers=l, pool_fn=args.pool_fn)
+                    acc = model.fit(
+                        data=data,
+                        device=device,
+                        tqdm_prefix=f'h={h},l={l},o={o},a={a}',
+                        **kwargs
+                    )
 
-                            acc = model.fit(
-                                data=data,
-                                device=device,
-                                tqdm_prefix=f'h={h},l={l},o={o},t={t},r={r},a={a}',
-                                **kwargs
-                            )
-
-                            result_record = {
-                                'order': o,
-                                'num_trees': t,
-                                'repetition': r,
-                                'assignment': a,
-                                'hidden_dim': h,
-                                'layers': l,
-                                'accuracy': acc
-                            }
-                            results.append(result_record)
-                            df = pd.DataFrame(results)
-                            df.to_csv(csv_path)
+                    result_record = {
+                        'order': o,
+                        'assignment': a,
+                        'hidden_dim': h,
+                        'layers': l,
+                        'accuracy': acc
+                    }
+                    results.append(result_record)
+                    df = pd.DataFrame(results)
+                    df.to_csv(csv_path)
